da1de9ac4f8b66c0dd7523778cfd1e398a7d8781
RocioGallardo01 <126356001+RocioGallardo01@users.noreply.github.com>
2023-02-27 14:38:17 -0800

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,15 @@
+
+document.getElementById("boton").onclick =function () {  
+     console.log("capturamos el evento");
+     document.getElementById("demo").innerHTML = "Desarroladora Front End con 5 años de experiencia dirigienndo proyectos de paginas web.Centrado en la creacion de códigos claros. Con capacidad para cumplir obejetivos y las necesidades del cliente.";
+    
+} 
+document.getElementById("boton-ocultar").addEventListener("click",function () {  
+
+    document.getElementById("demo").style.display = "none";
+   
+} );
+/**Colores de las fechas */
+document.getElementById("p1").style.color="brown";
+document.getElementById("p2").style.color="brown";
+
